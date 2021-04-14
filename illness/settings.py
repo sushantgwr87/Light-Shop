c931beb3410ee2ee12c6bba2febd0812cea06ac4
Sushant Gangwar <sushantgangwar87@gmail.com>
2021-04-15 04:37:51 +0530

    Added Procfile

--- illness/settings.py
+++ illness/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://lightsgwr.herokuapp.com/']
 
 
 # Application definition
