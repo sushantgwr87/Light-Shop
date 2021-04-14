35c8e0813bc5f3115ea61b01c7f7021eaae5864a
Sushant Gangwar <sushantgangwar87@gmail.com>
2021-04-15 04:24:00 +0530

    Updated settings

--- illness/settings.py
+++ illness/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
