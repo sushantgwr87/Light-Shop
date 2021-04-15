b1a2a2f553378da457db8b0d1b84278c461bb803
Sushant Gangwar <sushantgangwar87@gmail.com>
2021-04-15 11:09:08 +0530

    Updated Settings

--- illness/wsgi.py
+++ illness/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 # from whitenoise.django import DjangoWhiteNoise
 
@@ -17,3 +18,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'illness.settings')
 
 application = get_wsgi_application()
 # application = DjangoWhiteNoise(application)
+application = WhiteNoise(application, root='/static')
+application.add_files('/media', prefix='more-files/')
